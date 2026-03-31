a72253bf33d63870aeb6b4b93d0e3e6d6f96e5b1
Hosein Erfan <hosein.hosein2011@gmail.com>
2026-03-31 09:51:11 -0400

    Remove pkg_resources dependency from setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
-import os
-import pkg_resources
+from pathlib import Path
 from setuptools import setup, find_packages
+
+
+def read_requirements(requirements_path: Path):
+    requirements = []
+    for line in requirements_path.read_text().splitlines():
+        line = line.strip()
+        if not line or line.startswith("#"):
+            continue
+        requirements.append(line)
+    return requirements
 
 setup(
     name="splice",
@@ -10,10 +19,5 @@ setup(
     author_email="aoesterling@g.harvard.edu, usha_bhalla@g.harvard.edu",
     py_modules=["splice"],
     packages=find_packages(exclude=["experiments*", "data*"]),
-    install_requires=[
-        str(r)
-        for r in pkg_resources.parse_requirements(
-            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
-        )
-    ],
+    install_requires=read_requirements(Path(__file__).with_name("requirements.txt")),
 )
